523a8a75ce3e99f2ee7f5439ea7ebdec6fe1b0db
papoyyyyyyyyyy <athalla.kamil17@gmail.com>
2025-05-21 14:06:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as at
 
 st.title("Selamat Datang Warga 20 🥳")
 st.write(
-    "Selamat Ngoding guys🤩🥳!!!."
+    "Selamat Ngoding guys🤩🥳🥺!!."
 )
 st.image(IMG-20250521-WA0008.jpg, width=200)
