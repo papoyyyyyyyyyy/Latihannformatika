22b360db48f20fa2df84e9e3f3eea97eeb31a2ff
papoyyyyyyyyyy <athalla.kamil17@gmail.com>
2025-05-21 13:57:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,6 @@ import streamlit as st
 
 st.title("Gambar Prabowo Subianto")
 
-image_url = "https://upload.wikimedia.org/wikipedia/commons/6/6f/Prabowo_Subianto_2023.jpg"
-st.image(image_url, caption="Prabowo Subianto", use_column_width=True)
-
 st.title("Selamat Datang Warga 20 🥳")
 st.write(
     "Selamat Ngoding guys!!!."
